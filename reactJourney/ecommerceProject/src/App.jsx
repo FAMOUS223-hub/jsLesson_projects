45f6d28cc3c70c454632fb0a75a360e70bb29354
AMOS NGANBE <nganbewuborijaamos@gmail.com>
2026-09-24 12:26:51 +0000

    created multiple pages using react-router

--- reactJourney/ecommerceProject/src/App.jsx
+++ reactJourney/ecommerceProject/src/App.jsx
@@ -1,8 +1,12 @@
+import { Routes, Route } from 'react-router';
 import HomePage  from './pages/HomePage'
 const App = () => {
   return (
    <>
-    <HomePage />
+   <Routes>
+    <Route path='/' element = { <HomePage />} /> {/* path can be changed to 'index' tp mean path='/' */}
+    <Route path='checkout' element = {<div>Test Checkout</div>} />
+   </Routes>
    </>
   )
 }

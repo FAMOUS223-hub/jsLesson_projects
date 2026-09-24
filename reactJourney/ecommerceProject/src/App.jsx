0f082cc68d63bd5c7220ded97bd95765d52b2ef2
AMOS NGANBE <nganbewuborijaamos@gmail.com>
2026-09-24 12:43:36 +0000

    Added my checkout page with it styles

--- reactJourney/ecommerceProject/src/App.jsx
+++ reactJourney/ecommerceProject/src/App.jsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from 'react-router';
 import HomePage  from './pages/HomePage'
+import CheckOut from './pages/CheckOut'
 const App = () => {
   return (
    <>
    <Routes>
     <Route path='/' element = { <HomePage />} /> {/* path can be changed to 'index' tp mean path='/' */}
-    <Route path='checkout' element = {<div>Test Checkout</div>} />
+    <Route path='checkout' element = {<CheckOut />} />
    </Routes>
    </>
   )

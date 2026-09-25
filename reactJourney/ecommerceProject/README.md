e3be8f15255fff3602dc45919404a6e222d7b11d
AMOS NGANBE <nganbewuborijaamos@gmail.com>
2026-09-25 13:59:02 +0000

    added a few comments

--- reactJourney/ecommerceProject/README.md
+++ reactJourney/ecommerceProject/README.md
@@ -2,3 +2,4 @@
 
 <h1>Routing<j1/>
     path url can be changed to index to mean '/'
+    Learning and Building
--- reactJourney/ecommerceProject/src/App.jsx
+++ reactJourney/ecommerceProject/src/App.jsx
@@ -8,6 +8,7 @@ const App = () => {
    <>
    <Routes>
     <Route path='/' element = { <HomePage />} /> {/* path can be changed to 'index' tp mean path='/' */}
+    {/* I will work on this before i continue */}
     <Route path='checkout' element = {<CheckOut />} />
     <Route path='orders' element = {<Orders />} />
     <Route path='tracking' element = {<TrackingPage />} />
